c460b285cb3653cd19f860ffee7d8211da85e592
Matthew Carson <matthew@mac.mynetworksettings.com>
2025-03-25 12:11:05 -0400

    First Test

---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mcarson24\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function test_it_returns_a_random_joke() {
+        $this->assertTrue(true);
+    }
+}
